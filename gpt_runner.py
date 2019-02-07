9e2f05ac788ceea072e5ef8e7f100b079e728813
Shaun Cullen <shaun.cullen@canada.ca>
2019-02-07 11:35:43 -0500

    added gpt_graphs and test gpt runner in test dir

---
+++ gpt_runner.py
@@ -0,0 +1,146 @@
+"""Runs a subprocess and calls the bash script with the appropriate command line
+arguments.
+
+graph processsing toolkit can be run from the command line using GPT
+
+using the setup bash and bat scripts, this python script will:
+
+# Take appropriate args (product location, window size, target location)
+# Generate .properties file
+# Run a subporcess that calls the processDataset(.bash|.bat) with the positional args it requires
+# Report the success or failure of the process based on the stdout and stderr of the bash script
+
+"""
+
+from pathlib import Path
+import subprocess
+
+import asyncio
+
+
+class GPTRunner():
+    def __init__(self, product_path, target_path, graph_xml_file, arg_dict, process_id):
+
+        self.product_path = product_path
+        self.product_name = Path(self.product_path).name.split('.')[0]
+
+        self.graph_xml_file = graph_xml_file
+
+        self.target_path = target_path
+        self.arg_dict = arg_dict
+        self.process_id = process_id
+
+
+    def generate_properties_file(self):
+        """Based on the arg dict, create a properties file that GPT tool can read
+
+            sourceProductManifestFile = absolute path to Manifest.safe file in product dir
+
+            filterSize = x and y size of Gamma Map filter 7, 9 or 11 are good choices
+            windowSize = f'{filterSize}x{filterSize} string representing window size based on filter
+            bitDepth = target bitdepth of bands (float32 or int16)
+            outputTargetFilePath = abs file path to target file name (no suffix)
+        """
+
+        source_product_manifest_file_str = 'sourceProductManifestFile'
+        source_product_manifest_file_val = str(Path(self.product_path))
+
+        filter_size_str = 'filterSize'
+        filter_size_val = str(self.arg_dict['filterSize'])
+
+        window_size_str = 'windowSize'
+        window_size_val = f"{self.arg_dict['filterSize']}x{self.arg_dict['filterSize']}"
+
+        bit_depth_str = 'bitDepth'
+        bit_depth_val = self.arg_dict['bitDepth']
+
+        output_target_file_path_str = 'outputTargetFilePath'
+
+        target_suffix = '_' + Path(self.graph_xml_file).name.split('.')[0] + '.tif'
+        target_file_name = self.product_name + target_suffix
+
+        output_target_file_path_val = str(Path(self.target_path, target_file_name))
+
+        with open(f'process{self.process_id}.properties', 'w') as f:
+            f.write(f"{source_product_manifest_file_str}={source_product_manifest_file_val}\n")
+            f.write(f"{filter_size_str}={filter_size_val}\n")
+            f.write(f"{window_size_str}={window_size_val}\n")
+            f.write(f"{bit_depth_str}={bit_depth_val}\n")
+            f.write(f"{output_target_file_path_str}={output_target_file_path_val}\n")
+
+
+    async def _read_stream(stream, cb, result_string, ws):
+
+        # logging.basicConfig(level=logging.DEBUG, format="%(asctime)s-%(thread)d-%(process)d-%(funcName)s-%(message)s")
+        # logger.setFormatter(formatter)
+        # ch.setFormatter(formatter)
+
+        while True:
+            line = await stream.readline()
+            if line:
+                cb(line)  # just print the line in a simple lambda
+                # await logger.info(line.decode('utf-8').strip())
+
+                result_string.append(line.decode('utf-8').strip())
+            else:
+                break
+
+
+    async def run(self, cmd):
+
+        print(cmd)
+
+        proc = await asyncio.create_subprocess_exec(
+            *cmd,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE)
+
+        result_string = []
+        result_err_string = []
+
+        final_result = False
+
+        await asyncio.wait([
+                            _read_stream(proc.stdout, lambda x: print(f"STDOUT: {x.decode('utf-8').strip()}"), result_string, None),
+                            _read_stream(proc.stderr, lambda x: print(f"STDERR: {x.decode('utf-8').strip()}"), result_err_string, None)
+                        ])
+
+        print(f'[{cmd!r} exited with {proc.returncode}]')
+
+        if len(result_string) > 0:
+            # print(f'[stdout]\n{stdout.decode()}')
+            print(result_string)
+        if len(result_err_string) > 0:
+            # print(f'[stderr]\n{stderr.decode()}')
+            print(result_err_string)
+
+
+    def run_graph(self):
+        gpt_path = Path('/home/cullens/Development/sentinel_downloader/gpt_graphs')
+        properties_path = Path('/home/cullens/Development/sentinel_downloader/', f'process{self.process_id}.properties')
+
+        self.generate_properties_file()
+
+        bash_script_path = Path(gpt_path, 'processDataset.bash')
+        graph_xml_path = Path(gpt_path, 's1', 'ao_co_sf_tc_flt32_all.xml')
+
+        asyncio.run(self.run(
+            f'{bash_script_path} {self.graph_xml_file} {properties_path}'.split(' ')
+        ))
+
+if __name__ == "__main__":
+    pass
+    # product_path_arg = '/home/cullens/Development/s2d2/temp/S1B_IW_GRDH_1SDV_20180504T001446_20180504T001511_010764_013ABB_0FBB.SAFE'
+    # target_path_arg = '/home/cullens/Development/s2d2/temp'
+    # path_to_graph_xml = '/home/cullens/Development/sentinel_downloader/gpt_graphs/s1/ao_co_sf_tc_flt32_all.xml'
+
+    # arg_dict = {
+    #         'filterSize': 7,
+    #         'bitDepth': 'float32'
+    # }
+
+    # runner = gpt_runner.GPTRunner(product_path_arg,
+    #                               target_path_arg,
+    #                               path_to_graph_xml,
+    #                               arg_dict,
+    #                               1)
---
+++ test/test_gpt_runner.py
@@ -0,0 +1,208 @@
+import unittest
+from pathlib import Path
+import os
+
+import gpt_runner
+import datetime
+
+
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+
+class TestGPTRunner(unittest.TestCase):
+
+    def setUp(self):
+        self.product_path_arg = '/home/cullens/Development/s2d2/temp/S1B_IW_GRDH_1SDV_20180504T001446_20180504T001511_010764_013ABB_0FBB.SAFE'
+        self.target_path_arg = '/home/cullens/Development/s2d2/temp'
+        self.path_to_graph_xml = '/home/cullens/Development/sentinel_downloader/gpt_graphs/s1/ao_co_sf_tc_flt32_all.xml'
+
+
+        self.arg_dict = {
+            'filterSize': 7,
+            'bitDepth': 'float32'
+        }
+
+    def test_initial_variable_creation(self):
+
+        runner = gpt_runner.GPTRunner(self.product_path_arg,
+                                      self.target_path_arg,
+                                      self.path_to_graph_xml,
+                                      self.arg_dict,
+                                      1)
+
+        self.assertEquals(runner.product_name, 'S1B_IW_GRDH_1SDV_20180504T001446_20180504T001511_010764_013ABB_0FBB')
+
+    def test_properties_file_creation(self):
+
+        runner = gpt_runner.GPTRunner(self.product_path_arg,
+                                      self.target_path_arg,
+                                      self.path_to_graph_xml,
+                                      self.arg_dict,
+                                      1)
+
+        runner.generate_properties_file()
+
+        self.assertTrue(Path('./process1.properties').is_file())
+
+
+    def test_run_processing(self):
+        runner = gpt_runner.GPTRunner(self.product_path_arg,
+                                      self.target_path_arg,
+                                      self.path_to_graph_xml,
+                                      self.arg_dict,
+                                      1)
+
+        runner.generate_properties_file()
+        date_start = datetime.datetime.now()
+        runner.run_graph()
+        date_end = datetime.datetime.now()
+
+        time_elapsed = date_end - date_start
+        d = {}
+        d["hours"], rem = divmod(time_elapsed.seconds, 3600)
+        d["minutes"], d["seconds"] = divmod(rem, 60)
+        print(d)
+
+
+    # def test_generate_properties_file(self):
+    #     results = self.downloader_obj.search_for_products_polygon_to_tiles(self.SENTINEL2_DATASET_NAME,
+    #                                                             self.test_footprint_1,
+    #                                                             self.QUERY_DICT_EXAMPLE,
+    #                                                             detailed=True)
+
+    #     print(len(results))
+    #     self.assertTrue(True)
+
+    # def test_search_for_products_polygon_to_tiles_abagextent(self):
+    #     results = self.downloader_obj.search_for_products_polygon_to_tiles(self.SENTINEL2_DATASET_NAME,
+    #                                                             self.test_footprint_1,
+    #                                                             self.QUERY_DICT_EXAMPLE,
+    #                                                             detailed=True)
+
+    #     print(len(results))
+    #     self.assertTrue(True)
+
+    # def test_search_for_products_polygon_to_tiles_sentinel2(self):
+    #     results = self.downloader_obj.search_for_products(self.SENTINEL2_DATASET_NAME,
+    #                                                       self.test_footprint_3_lethbridge,
+    #                                                       self.QUERY_DICT_EXAMPLE)
+
+    #     print(len(results))
+    #     print(results[0])
+    #     print(results)
+    #     single_element_list = []
+    #     single_element_list.append(results[0])
+    #     print(single_element_list)
+    #     # populated_result = self.downloader_obj.populate_result_list(single_element_list,
+    #     #                                                             self.SENTINEL2_PLATFORM_NAME,
+    #     #                                                             self.SENTINEL2_DATASET_NAME)
+    #     # print(populated_result)
+    #     self.assertTrue(True)
+
+    # @timeit
+    # def test_get_dataset_metadata_info_sentinel2(self):
+
+    #     results = self.downloader_obj.get_dataset_field_ids(self.SENTINEL2_DATASET_NAME)
+
+    #     # print(results)
+    #     self.assertTrue(True)
+
+    # @timeit
+    # def test_populate_result_list(self):
+    #     # Load previous results
+    #     json_results = None
+    #     with open(self.path_to_intermediate_query_results_usgs_ee, 'r') as json_file:
+    #         json_results = json.load(json_file)
+
+    #     print(json_results)
+
+    #     cleaned_results = self.downloader_obj.populate_result_list(json_results, self.SENTINEL2_PLATFORM_NAME, self.SENTINEL2_DATASET_NAME, detailed=False)
+
+    #     cleaned_results_compare = None
+    #     with open(self.path_to_cleaned_query_results_usgs_ee, 'r') as outfile:
+    #         clean_results_compare = json.load(outfile)
+
+    #     self.assertEqual(clean_results_compare, cleaned_results)
+
+    # @timeit
+    # def test_search_scene_metadata(self):
+    #     json_results = None
+    #     with open(self.path_to_intermediate_query_results_usgs_ee, 'r') as json_file:
+    #         json_results = json.load(json_file)
+
+    #     data_results = json_results['data']['results']
+    #     just_entity_ids = [d['entityId'] for d in data_results]
+
+    #     print(just_entity_ids)
+
+    #     detailed_metadata_results = self.downloader_obj.search_scene_metadata(self.SENTINEL2_DATASET_NAME,
+    #                                                                           just_entity_ids)
+
+    #     print(detailed_metadata_results)
+    #     self.assertEqual(len(detailed_metadata_results), 12)
+
+    # @timeit
+    # def test_search_for_products_by_tile(self):
+    #     results = self.downloader_obj.search_for_products_by_tile(self.SENTINEL2_DATASET_NAME,
+    #                                                     self.tile_list_small,
+    #                                                     self.QUERY_DICT_EXAMPLE,
+    #                                                     detailed=True)
+
+    #     print(results)
+    #     self.assertEqual(len(results), 12)
+
+    # def test_alberta_ag_extent(self):
+
+    #     results = self.downloader_obj.search_for_products_polygon_to_tiles(
+    #                 self.SENTINEL2_DATASET_NAME,
+    #                 self.test_footprint_2,
+    #                 self.QUERY_DICT_EXAMPLE,
+    #                 detailed=True)
+
+    #     self.assertEqual(len(results), 988)
+
+    # @timeit
+    # def test_search_for_products_by_tile_detailed(self):
+    #     results = self.downloader_obj.search_for_products_by_tile(self.SENTINEL2_DATASET_NAME,
+    #                                                     self.tile_list_small,
+    #                                                     self.query_dict_example_single_tile,
+    #                                                     detailed=True)
+
+    #     print(results)
+    #     self.assertEqual(len(results), 1)
+
+    # @timeit
+    # def test_search_for_products_by_tile_not_detailed(self):
+    #     results = self.downloader_obj.search_for_products_by_tile(self.SENTINEL2_DATASET_NAME,
+    #                                                     self.tile_list_small,
+    #                                                     self.query_dict_example_single_tile,
+    #                                                     detailed=False)
+
+    #     print(results)
+    #     self.assertEqual(len(results), 1)
+
+    # # def test_find_mgrs_intersection_coarse(self):
+    # #     result_list = utilities.find_mgrs_intersection_large(self.test_footprint_2)
+
+    # #     self.assertEqual(set(result_list), set(self.test_footprint_2_result_list))
+
+    # # def test_find_mgrs_intersection_fine_single(self):
+
+    # #     single_gzd = '12U'
+
+    # #     fine_result_list_single = utilities.find_mgrs_intersection_100km_single(self.test_footprint_2,
+    # #                                                                             single_gzd)
+
+    # #     self.assertEqual(set(fine_result_list_single), set(self.test_footprint_2_result_list_single))
+
+    # # def test_find_mgrs_intersection_fine(self):
+
+    # #     gzd_initial_list = self.test_footprint_2_result_list
+
+    # #     fine_result_list = utilities.find_mgrs_intersection_100km(self.test_footprint_2,
+    # #                                                               gzd_initial_list)
+
+    # #     self.assertEqual(set(fine_result_list), set(self.test_footprint_2_result_list2))
+
+
+if __name__ == '__main__':
+    unittest.main()
