5be73842551ea4c87824f2793712afa8a8d2cdfd
Shaun Cullen <ss.cullen@uleth.ca>
2019-09-11 15:58:05 -0600

    problem with a Path object trying to be json encoded

--- sentinel_downloader/s2_downloader.py
+++ sentinel_downloader/s2_downloader.py
@@ -346,10 +346,10 @@ class S2Downloader:
                     False, "An exception occured while trying to download.", e
                 )
             else:
-                return TaskStatus(True, "Download successful", full_file_path)
+                return TaskStatus(True, "Download successful", str(full_file_path))
         else:
             return TaskStatus(
-                True, "Requested file to download already exists.", full_file_path
+                True, "Requested file to download already exists.", str(full_file_path)
             )
 
     def download_file(self, url, download_name, download_id):
