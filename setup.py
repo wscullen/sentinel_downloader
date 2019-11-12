1b7885c22b880296d1f6a380824966d8efa97de1
sscullen <ss.cullen@uleth.ca>
2019-11-12 15:23:00 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sentinel_downloader',
+      version='0.0.1',
+      description='Utilities for downloading Landsat and Sentinel products from USGS',
+      url='https://github.com/sscullen/sentinel_downloader.git',
+      author='Shaun Cullen',
+      author_email='ss.cullen@uleth.ca',
+      license='MIT',
+      packages=['sentinel_downloader'],
+      zip_safe=False)
